45afa5a2f6e960baaa87e1f971ffa62f79e6ef8c
calin.g.sulea@gmail.com <wallstr33t153>
2018-12-08 16:22:31 +0200

    [dev] Added publish, update and delete news to server

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -27,15 +27,36 @@ public class Server {
         return instance;
     }
 
-
     private void publishNews(News news, NewsEventListener listener){
         synchronized(mutex){
             newsList.addFirst(news);
         }
         eventDispatcher.register(NewsEventType.NEWS_READ, listener);
-        NewsEvent event = new NewsEvent(NewsEventType.NEWS_APPEARED, news);
 
+        NewsEvent event = new NewsEvent(NewsEventType.NEWS_APPEARED, news);
         eventDispatcher.publishEvent(event);
+    }
+
+    private void updateNews(News news){
+        synchronized(mutex){
+            newsList.stream().filter(news::equals).map(item -> news);
+            NewsEvent event = new NewsEvent(NewsEventType.NEWS_CHANGED, news);
+            eventDispatcher.publishEvent(event);
+        }
+    }
+
+    private void deleteNews(News news){
+        synchronized(mutex){
+            newsList.removeIf(news::equals);
+        }
+        NewsEvent event = new NewsEvent(NewsEventType.NEWS_DELETED, news);
+        eventDispatcher.publishEvent(event);
+    }
+
+    private void subscribeToNews(NewsEventListener listener, String newsType){
+        eventDispatcher.register(NewsEventType.NEWS_CHANGED, listener);
+        eventDispatcher.register(NewsEventType.NEWS_APPEARED, listener);
+        eventDispatcher.register(NewsEventType.NEWS_DELETED, listener);
     }
 
     private void serverLoop(){
